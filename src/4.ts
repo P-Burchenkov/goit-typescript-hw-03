50655217f48beffcf77d90e11ce291e1782ab3ba
P-Burchenkov <p.burchenkov@ukr.net>
2023-11-01 21:43:51 +0200

    ready all asigntments

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+    private signature = Math.random();
+    getSignature(): number{
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key){}
+    getKey(): Key{
+        return this.key;
+    }
+}
+
+abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[];
+    abstract openDoor(key: Key): void;
+    comeIn(person: Person): void{
+        if (this.door) {
+          this.tenants.push(person)  
+        }
+    }
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+    openDoor(key: Key): void {
+        if (key===this.key) {
+            this.door = true;
+        } else {
+            this.door === false
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
